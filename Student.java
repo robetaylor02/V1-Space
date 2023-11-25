0da5d7fcd4eefc3d56a4caa35b4a71efc27e26e3
BOFACAM <55903105+BOFACAM@users.noreply.github.com>
2023-11-25 16:09:39 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,49 +1,115 @@
 import java.util.Scanner;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
-
-
 public class Student {
-    public static void generateStudentReports() {
+    private static boolean menu = true;
+    public static void generateStudentReports(Scanner scnr) {
         // Implement code to generate reports available to students
+        
+        while(menu){
         System.out.println("Student Reports:");
         System.out.println("1. Applicant Report");
         System.out.println("2. Application Status Report");
         System.out.println("3. Scholarship Match Report");
-        // Add more student-specific reports as needed
+        System.out.println("4.Go back to main menu");
 
-        int reportChoice = ReportEngineTest.getReportChoice();
-        generateStudentReport(reportChoice);
+        int reportChoice = ReportEngineTest.getReportChoice(scnr);
+        generateStudentReport(reportChoice, scnr);
+        
     }
+}
 
-    private static void generateStudentReport(int choice) {
-        // Implement logic to generate the selected student report
+    private static void generateStudentReport(int choice, Scanner scnr){
 
-        switch(choice){
+        switch (choice) {
             case 1:
-                generateApplicantReport();
+                generateApplicantReport(scnr);
                 break;
-            //add other reports
+            // add application status report and scholarship match report
+            case 2: 
+                genrateApplicationStatusReport(scnr);
+                break;
+            case 4:
+                menu = false;
+                break;
             default:
-            System.out.println("Wrong report choice for student.");
-
+                System.out.println("Wrong report choice for student.");
         }
     }
-    private static void generateApplicantReport(){
+
+    private static void generateApplicantReport(Scanner scnr) {
         try (BufferedReader personalInfo = new BufferedReader(new FileReader("personal_info.txt"));
-             BufferedReader awardedScholarshipsInfo = new BufferedReader(new FileReader("awarded_scholarships.txt"))) {
-            
+             BufferedReader awardedScholarshipsInfo = new BufferedReader(new FileReader("awarded_scholarships.txt"));
+             BufferedWriter applicantReportoutputFile = new BufferedWriter(new FileWriter("applicant_report.txt"))){
+
             System.out.println("Enter your name: ");
-            String StudentName = ReportEngineTest.scnr.nextLine();//not sure about this
+            scnr.nextLine();
+            String studentName = scnr.nextLine();
 
-            //read file personal info
-            string StudentPersonalInfoLine;
-            
-            while(StudentPersonalInfoLine = )
+            // read file personal info
+            String studentPersonalInfoLine;
 
-             
+            while ((studentPersonalInfoLine = personalInfo.readLine()) != null) {
+                String[] student = studentPersonalInfoLine.split(",");
+                if (student[0].equalsIgnoreCase(studentName.trim())) {
+                    applicantReportoutputFile.write("Applicant Report for " + student[0] + ":\n");
+                    applicantReportoutputFile.write("===================================\n");
+                    applicantReportoutputFile.write("Name: " + student[0] + "\n");
+                    applicantReportoutputFile.write("GPA: " + student[1] + "\n");
+                    applicantReportoutputFile.write("Intended Graduation Year: " + student[2] + "\n");
+                    applicantReportoutputFile.write("Major: " + student[3] + "\n\n");
+                    break;
+                }
             }
+
+            applicantReportoutputFile.write("Previously awarded scholarships:\n ");
+            String awardedScholarshipsLine;
+
+            while ((awardedScholarshipsLine = awardedScholarshipsInfo.readLine()) != null) {
+                String[] scholarships = awardedScholarshipsLine.split(",");
+                if (scholarships[0].equalsIgnoreCase(studentName.trim())) {
+                    for (int i = 1; i < scholarships.length; i++) {
+                        applicantReportoutputFile.write("-" + scholarships[i] + "\n");
+                    }
+                    break;
+                }
+            }
+            System.out.println("Applicant report generated and saved to 'applicant_report.txt'");
+        } catch (IOException except) {
+            except.printStackTrace();
         }
+    }
+
+    public static void genrateApplicationStatusReport(Scanner scnr){
+        try (BufferedReader scholarshipStatus = new BufferedReader(new FileReader("ScholarshipStatus.txt"));
+            BufferedWriter applicantStatusReportOutputFile = new BufferedWriter(new FileWriter("applicantstatus_report.txt"))) {
+
+            System.out.println("Enter applicant name: ");
+            scnr.nextLine();
+            String StudentName = scnr.nextLine();
+
+            String scholarshipline;
+
+            while((scholarshipline = scholarshipStatus.readLine()) != null){
+                String[] scholarship = scholarshipline.split(",");
+                if(scholarship[0].equalsIgnoreCase(StudentName.trim())){
+                    applicantStatusReportOutputFile.write("Application Status Report for " + scholarship[0] + ":\n");
+                    applicantStatusReportOutputFile.write("===================================\n");
+                    for (int i = 1; i < scholarship.length; i++) {
+                        applicantStatusReportOutputFile.write(scholarship[i] + "\n");
+                    }
+                    break;
+                }
+
+            }
+
+            System.out.println("Application status report generated and saved to 'applicantstatus_report.txt'");
+        }catch(IOException except){
+                except.printStackTrace();
+            }     
+    }
 }
