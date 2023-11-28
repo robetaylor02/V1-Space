034986901b2e35a8b640b163061cd66c34413e6e
BOFACAM <55903105+BOFACAM@users.noreply.github.com>
2023-11-28 00:39:12 -0700

    Update ReportEngineTest.java
    with error handling

--- ReportEngineTest.java
+++ ReportEngineTest.java
@@ -1,44 +1,54 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ReportEngineTest {
     private static Scanner scnr = new Scanner(System.in);
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         boolean exit = false;
 
-        while(!exit){
-        System.out.println("Choose what user type you are:");
-        System.out.println("1. Student");
-        System.out.println("2. Reviewer");
-        System.out.println("3. Admin");
-        System.out.println("4. Donor");
-        System.out.println("5.Exit");
-        System.out.println("Enter number: ");
+        while (!exit) {
+            System.out.println("Choose what user type you are:");
+            System.out.println("1. Student");
+            System.out.println("2. Reviewer");
+            System.out.println("3. Admin");
+            System.out.println("4. Donor");
+            System.out.println("5. Exit");
+            System.out.println("Enter number: ");
 
-        int userChoice = scnr.nextInt();
-        scnr.nextLine();
+            try {
+                int userChoice = scnr.nextInt();
+                scnr.nextLine(); // Consume the newline character
 
-        switch (userChoice) {
-            case 1:
-                Student.generateStudentReports(scnr);
-                break;
-            case 2:
-                Reviewer.generateReviewerReports(scnr);
-                break;
-            case 3:
-                Admin.generateAdminReports(scnr);
-                break;
-            case 4:
-                Donor.generateDonorReports(scnr);
-                break;
-            case 5:
-                exit = true;
-            default:
-                System.out.println("Exiting...");
+                if (userChoice >= 1 && userChoice <= 4) {
+                    switch (userChoice) {
+                        case 1:
+                            Student.generateStudentReports(scnr);
+                            break;
+                        case 2:
+                            Reviewer.generateReviewerReports(scnr);
+                            break;
+                        case 3:
+                            Admin.generateAdminReports(scnr);
+                            break;
+                        case 4:
+                            Donor.generateDonorReports(scnr);
+                            break;
+                    }
+                } else if (userChoice == 5) {
+                    exit = true;
+                } else {
+                    System.out.println("Invalid input. Please enter a number between 1 and 5.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a numeric value.");
+                scnr.nextLine(); // Clear the invalid input
             }
         }
     }
-    public static int getReportChoice(Scanner scnr){
+
+    public static int getReportChoice(Scanner scnr) {
         System.out.println("Enter the number of the report you want to generate: ");
-            return scnr.nextInt();
+        return scnr.nextInt();
     }
 }
