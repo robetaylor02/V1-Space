ac11fc963c075a3de952c797f0d7f67edd5dab98
BOFACAM <55903105+BOFACAM@users.noreply.github.com>
2023-11-28 00:39:44 -0700

    Update Reviewer.java

--- Reviewer.java
+++ Reviewer.java
@@ -1,123 +1,142 @@
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.util.Scanner;
 
 public class Reviewer {
     private static boolean menu = true;
+
     public static void generateReviewerReports(Scanner scnr) {
-       
-        while(menu){
-        System.out.println("Reviewer Reports:");
-        System.out.println("1. Applicant Report");
-        System.out.println("2. Reviewable Applications Report");
-        System.out.println("3. Scholarship Summary Report");
-        System.out.println("4. Go back to main menu ");
-       
-        int reportChoice = ReportEngineTest.getReportChoice(scnr);
-        generateReviewerReport(reportChoice,scnr);
-    }
-}
+        while (menu) {
+            System.out.println("Reviewer Reports:");
+            System.out.println("1. Applicant Report");
+            System.out.println("2. Reviewable Applications Report");
+            System.out.println("3. Scholarship Summary Report");
+            System.out.println("4. Go back to main menu ");
 
-    private static void generateReviewerReport(int choice, Scanner scnr) {
+            int reportChoice = ReportEngineTest.getReportChoice(scnr);
+            if (reportChoice == 4) {
+                menu = true;
+                return;
+            }
 
-        switch(choice){
-            case 1:
-                generateReviewerApplicantReport(scnr);
-                break;
-            case 2:
-                generateReviewableApplicationsReport(scnr);
-                break;
-            case 3:
-                generateScholarshipSummaryReport(scnr);
-                break;
-            case 4:
-                menu = false;
-                break;
-            default:
-                System.out.println("Wrong report choice for reviewers");
-
-            
+            try {
+                generateReviewerReport(reportChoice, scnr);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
         }
+    }
 
-    public static void generateReviewerApplicantReport(Scanner scnr){
+    private static void generateReviewerReport(int choice, Scanner scnr) {
+        try {
+            switch (choice) {
+                case 1:
+                    generateReviewerApplicantReport(scnr);
+                    break;
+                case 2:
+                    generateReviewableApplicationsReport(scnr);
+                    break;
+                case 3:
+                    generateScholarshipSummaryReport(scnr);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Wrong report choice for reviewers");
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); // or handle the exception as needed
+        }
+    }
+    
+
+
+    public static void generateReviewerApplicantReport(Scanner scnr) {
         try (BufferedReader personalInfo = new BufferedReader(new FileReader("personal_info.txt"));
-        BufferedReader awardedScholarshipsInfo = new BufferedReader(new FileReader("awarded_scholarships.txt"));
-        BufferedWriter applicantReportoutputFile = new BufferedWriter(new FileWriter("reviewer_applicant_report.txt"))){
+             BufferedReader awardedScholarshipsInfo = new BufferedReader(new FileReader("awarded_scholarships.txt"));
+             BufferedWriter applicantReportoutputFile = new BufferedWriter(new FileWriter("reviewer_applicant_report.txt"))) {
 
-       System.out.println("Enter name of applicant: ");
-       scnr.nextLine();
-       String studentName = scnr.nextLine();
+            System.out.println("Enter name of student: ");
+            scnr.nextLine();
+            String studentName = scnr.nextLine();
 
-       String studentPersonalInfoLine;//read file
+            boolean studentFound = false;
 
-       while ((studentPersonalInfoLine = personalInfo.readLine()) != null) {
-           String[] student = studentPersonalInfoLine.split(",");
-           if (student[0].equalsIgnoreCase(studentName.trim())) {
-               applicantReportoutputFile.write("Applicant Report for " + student[0] + ":\n");
-               applicantReportoutputFile.write("===================================\n");
-               applicantReportoutputFile.write("Name: " + student[0] + "\n");
-               applicantReportoutputFile.write("GPA: " + student[1] + "\n");
-               applicantReportoutputFile.write("Intended Graduation Year: " + student[2] + "\n");
-               applicantReportoutputFile.write("Major: " + student[3] + "\n\n");
-               break;
-           }
-       }
+            String studentPersonalInfoLine;
+            while ((studentPersonalInfoLine = personalInfo.readLine()) != null) {
+                String[] student = studentPersonalInfoLine.split(",");
+                if (student[0].equalsIgnoreCase(studentName.trim())) {
+                    applicantReportoutputFile.write("Applicant Report for " + student[0] + ":\n");
+                    applicantReportoutputFile.write("===================================\n");
+                    applicantReportoutputFile.write("Name: " + student[0] + "\n");
+                    applicantReportoutputFile.write("GPA: " + student[1] + "\n");
+                    applicantReportoutputFile.write("Intended Graduation Year: " + student[2] + "\n");
+                    applicantReportoutputFile.write("Major: " + student[3] + "\n\n");
+                    studentFound = true;
+                    break;
+                }
+            }
 
-       applicantReportoutputFile.write("Previously awarded scholarships:\n ");
-       String awardedScholarshipsLine;
+            if (!studentFound) {
+                throw new IllegalArgumentException("Student not found. Please enter a valid name.");
+            }
 
-       while ((awardedScholarshipsLine = awardedScholarshipsInfo.readLine()) != null) {
-           String[] scholarships = awardedScholarshipsLine.split(",");
-           if (scholarships[0].equalsIgnoreCase(studentName.trim())) {
-               for (int i = 1; i < scholarships.length; i++) {
-                   applicantReportoutputFile.write("-" + scholarships[i] + "\n");
-               }
-               break;
-           }
-       }
-       System.out.println("Applicant report generated and saved to 'reviewer_applicant_report.txt'");
+            applicantReportoutputFile.write("Previously awarded scholarships:\n");
+            String awardedScholarshipsLine;
 
-   } catch (IOException except) {
-       except.printStackTrace();
-   }
+            while ((awardedScholarshipsLine = awardedScholarshipsInfo.readLine()) != null) {
+                String[] scholarships = awardedScholarshipsLine.split(",");
+                if (scholarships[0].equalsIgnoreCase(studentName.trim())) {
+                    for (int i = 1; i < scholarships.length; i++) {
+                        applicantReportoutputFile.write("- " + scholarships[i] + "\n");
+                    }
+                    break;
+                }
+            }
+            System.out.println("Applicant report generated and saved to 'reviewer_applicant_report.txt'");
 
-
+        } catch (IOException except) {
+            except.printStackTrace();
+        }
     }
 
     public static void generateReviewableApplicationsReport(Scanner scnr) {
         try (BufferedReader reviewerScholarships = new BufferedReader(new FileReader("ReviewerScholarships.txt"));
              BufferedWriter scholarshipsToReviewOutputFile = new BufferedWriter(new FileWriter("ScholarshipsToReview.txt"))) {
-    
+
             System.out.println("Enter applicant name to see reviewable scholarships: ");
             scnr.nextLine();
             String studentName = scnr.nextLine();
-    
+
+            boolean studentFound = false;
+
             String line;
             while ((line = reviewerScholarships.readLine()) != null) {
                 String[] review = line.split(",");
                 if (review.length >= 2 && review[0].equalsIgnoreCase(studentName.trim())) {
                     scholarshipsToReviewOutputFile.write("Scholarships to be reviewed for " + studentName + ":" + "\n");
-    
+
                     for (int i = 1; i < review.length; i++) {
                         String[] scholarshipInfo = review[i].split(":");
                         if (scholarshipInfo.length == 2) {
                             String scholarshipName = scholarshipInfo[0].trim();
                             String status = scholarshipInfo[1].trim();
-    
+
                             if (status.equalsIgnoreCase("Submitted")) {
                                 scholarshipsToReviewOutputFile.write("- " + scholarshipName + "\n");
-                                //scholarshipsToReviewOutputFile.write(studentName + "," + scholarshipName + "\n");
                             }
                         }
                     }
-                   //break;
+                    studentFound = true;
+                    break;
                 }
             }
+
+            if (!studentFound) {
+                throw new IllegalArgumentException("Student not found. Please enter a valid name.");
+            }
+
             System.out.println("Scholarships for " + studentName + " written to ScholarshipsToReview.txt");
 
         } catch (IOException except) {
@@ -125,8 +144,82 @@ public class Reviewer {
         }
     }
 
-    public static void generateScholarshipSummaryReport(Scanner scnr){
+    public static void generateScholarshipSummaryReport(Scanner scnr) throws IOException {
+        try (BufferedReader scholarshipData = new BufferedReader(new FileReader("ScholarshipData.txt"));
+             BufferedReader scholarshipStatus = new BufferedReader(new FileReader("ScholarshipStatus.txt"));
+             BufferedWriter scholarshipSummaryReport = new BufferedWriter(new FileWriter("ScholarshipSummaryReport.txt"))) {
 
+            System.out.println("Enter the name of the scholarship for the summary report: ");
+            scnr.nextLine();  // Consume the newline character
+            String scholarshipName = scnr.nextLine();
+
+            boolean scholarshipFound = false;
+
+            // Validate scholarship name
+            String dataLine;
+            while ((dataLine = scholarshipData.readLine()) != null) {
+                String[] data = dataLine.split("\\|");
+                if (data.length > 0 && data[0].trim().equalsIgnoreCase(scholarshipName)) {
+                    scholarshipFound = true;
+                    break;
+                }
+            }
+
+            if (!scholarshipFound) {
+                throw new IllegalArgumentException("Scholarship not found or invalid name. Please enter a valid scholarship name.");
+            }
+
+            scholarshipSummaryReport.write("Scholarship Summary Report for " + scholarshipName + ":\n");
+            scholarshipSummaryReport.write("============================\n");
+
+            int totalAmountAwarded = 0;
+
+            String scholarshipDataLine;
+            while ((scholarshipDataLine = scholarshipData.readLine()) != null) {
+                String[] data = scholarshipDataLine.split("\\|");
+                String currentScholarshipName = data[0].trim();
+                if (currentScholarshipName.equalsIgnoreCase(scholarshipName)) {
+                    totalAmountAwarded = Integer.parseInt(data[1].trim());
+                    break;  // Found the scholarship, no need to continue searching
+                }
+            }
+
+            scholarshipSummaryReport.write("Total Amount Awarded: $" + totalAmountAwarded + "\n");
+            scholarshipSummaryReport.write("Names of Recipients: " + getAwardedStudents(scholarshipName, scholarshipStatus) + "\n\n");
+
+            System.out.println("Scholarship Summary Report for " + scholarshipName + " generated and saved to 'ScholarshipSummaryReport.txt'");
+
+        } catch (IOException except) {
+            throw new IOException("Error", except);
+        }
     }
 
+    private static String getAwardedStudents(String scholarshipName, BufferedReader scholarshipStatus) throws IOException {
+        StringBuilder awardedStudents = new StringBuilder();
+
+        String scholarshipStatusLine;
+        while ((scholarshipStatusLine = scholarshipStatus.readLine()) != null) {
+            String[] status = scholarshipStatusLine.split(",");
+            String studentName = status[0].trim();
+
+            for (int i = 1; i < status.length; i++) {
+                String[] scholarshipInfo = status[i].split(":");
+                if (scholarshipInfo.length == 2) {
+                    String statusScholarshipName = scholarshipInfo[0].trim();
+                    String awardStatus = scholarshipInfo[1].trim();
+
+                    if (awardStatus.equalsIgnoreCase("Awarded") && statusScholarshipName.equalsIgnoreCase(scholarshipName)) {
+                        awardedStudents.append(studentName).append(", ");
+                    }
+                }
+            }
+        }
+
+        // Remove trailing comma and space
+        if (awardedStudents.length() > 0) {
+            awardedStudents.setLength(awardedStudents.length() - 2);
+        }
+
+        return awardedStudents.toString();
     }
+}
